23e93e9c3e15649c19507ecd81a36fdb1452ae3e
Mihail Vainer <mh93@yandex.ru>
2026-02-23 14:02:38 +0300

    Go! Go! Go!

---
+++ README.md
@@ -0,0 +1,1 @@
+Go Studygit init!
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello World!!!")
+	fmt.Print("Hello Brother!!!")
+	fmt.Print("Hello Brother2!!!")
+	fmt.Println("Hello Brother3!!!")
+}
